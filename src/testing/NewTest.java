e094778506429f0465f3e1d54cbfdbe72e39bf44
I1538 <I1538@ISILLT10372.indium.com>
2019-06-23 00:25:20 +0530

    MOdified Repository Files

---
+++ src/testing/NewTest.java
@@ -0,0 +1,24 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class NewTest 
+{
+  @Test
+  public void Test1() 
+  {
+	  System.out.println("Test 1");
+  }
+  
+  @Test
+  public void Test2() 
+  {
+	  System.out.println("Test 2");
+  }
+  
+  @Test
+  public void Test3() 
+  {
+	  System.out.println("Test 3");
+  }
+}
